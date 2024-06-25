f17564c746225fd525b6236d441eb81033c532c8
Sheys <95696361+Sheys11@users.noreply.github.com>
2024-06-25 08:43:46 -0700

    add more configs to providers

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -6,20 +6,31 @@ import { PrivyProvider } from "@privy-io/react-auth";
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <PrivyProvider
-      appId="clxkjt5d203bnppdob8nsgamf"
-      config={{
-        // Customize Privy's appearance in your app
-        appearance: {
-          theme: "light",
-          accentColor: "#676FFF",
-          logo: "",
-        },
-        defaultChain: base,
-        supportedChains: [base, baseSepolia],
-        // Create embedded wallets for users who don't have a wallet
-        embeddedWallets: {
-          createOnLogin: "users-without-wallets",
-        },
+    appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+    config={{
+      // Customize Privy's appearance in your app
+      appearance: {
+        theme: "light",
+        accentColor: "#676FFF",
+        logo: "",
+        walletList: ["coinbase_wallet"],
+      },
+
+      defaultChain: base,
+      supportedChains: [base, baseSepolia],
+
+      loginMethods: ["email", "wallet", "google", "apple"],
+      // Create embedded wallets for users who don't have a wallet - we might have to delay the use of embedded wallet to concentrate our calls through coinbase smart wallet at the moment
+      //embeddedWallets: {
+      //  createOnLogin: "users-without-wallets",
+      // },
+      
+      externalWallets: {
+          coinbaseWallet: {
+              //Connection options should integrate coinbase smart wallet only
+              connectionOptions: "smartWalletOnly",
+          },
+      },
       }}
     >
       {children}
