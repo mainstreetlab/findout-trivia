b1b34ce97244a02eb3538ff6824df93e4f795465
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2024-06-26 12:55:47 +0100

    integration: wagmi and tanstack-query providers

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -3,41 +3,48 @@ import {base, baseSepolia} from "viem/chains"
 
 import { PrivyProvider } from "@privy-io/react-auth";
 import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import {wagmiConfig} from "@/config"
 
+const queryClient = new QueryClient()
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <PrivyProvider
-    appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
-    config={{
-      // Customize Privy's appearance in your app
-      appearance: {
-        theme: "light",
-        accentColor: "#676FFF",
-        logo: "",
-        walletList: ["coinbase_wallet"],
-      },
-
-      defaultChain: base,
-      supportedChains: [base, baseSepolia],
-
-      loginMethods: ["email", "wallet", "google", "apple"],
-      // Create embedded wallets for users who don't have a wallet - we might have to delay the use of embedded wallet to concentrate our calls through coinbase smart wallet at the moment
-      //embeddedWallets: {
-      //  createOnLogin: "users-without-wallets",
-      // },
-      
-      externalWallets: {
-          coinbaseWallet: {
-              //Connection options should integrate coinbase smart wallet only
-              connectionOptions: "smartWalletOnly",
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <PrivyProvider
+        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+        config={{
+          // Customize Privy's appearance in your app
+          appearance: {
+            theme: "light",
+            accentColor: "#676FFF",
+            logo: "",
+            walletList: ["coinbase_wallet"],
           },
-      },
-      }}
-    >
-      {children}
-    </PrivyProvider>
+
+          defaultChain: base,
+          supportedChains: [base, baseSepolia],
+
+          loginMethods: ["email", "wallet", "google", "apple"],
+          // Create embedded wallets for users who don't have a wallet - we might have to delay the use of embedded wallet to concentrate our calls through coinbase smart wallet at the moment
+          //embeddedWallets: {
+          //  createOnLogin: "users-without-wallets",
+          // },
+          
+          externalWallets: {
+              coinbaseWallet: {
+                  //Connection options should integrate coinbase smart wallet only
+                  connectionOptions: "smartWalletOnly",
+              },
+          },
+          }}
+        >
+          {children}
+        </PrivyProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 };
 
