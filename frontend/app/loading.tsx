3952778294cc7da232a8640c86431f306bc859f0
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2024-07-04 00:50:25 +0100

    build(ui): add loading component

---
+++ frontend/app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      {/* Add your loading indicator here (e.g., spinner, text) */}
+      <p>Loading...</p>
+    </div>
+  );
+}
