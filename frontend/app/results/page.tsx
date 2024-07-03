1c1f79ee303f4963a1b14754cb59b3a469f4c6f8
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2024-07-04 00:45:23 +0100

    fix: retry results page usesearchparams fix

--- frontend/app/results/page.tsx
+++ frontend/app/results/page.tsx
@@ -2,19 +2,16 @@
 
 import Results from "@/components/Results";
 import { Suspense } from 'react';
-import { useSearchParams } from 'next/navigation';
 
 const ResultsPage = () => {
-  const searchParams = useSearchParams();
-  const id = searchParams.get('id');
   return (
-    <Suspense>
-      <main className=" pt-28">
-        <div className="container mx-auto flex flex-col justify-center h-full items-center">
-          <Results id={id} />
-        </div>
-      </main>
-    </Suspense>
+    <main className=" pt-28">
+      <div className="container mx-auto flex flex-col justify-center h-full items-center">
+        <Suspense>
+          <Results />
+        </Suspense>
+      </div>
+    </main>
   );
 };
 
